5bb65c12b42ea9c1b5818565b94d4fe04dd918b7
SystemSourcer <157804367+SystemSourcer@users.noreply.github.com>
2024-10-08 12:22:08 +0200

    Update pedigree.py
    Add imports

--- pedigree.py
+++ pedigree.py
@@ -2,3 +2,13 @@
 Programmbeschriebung:
 Programm zum erstellen eines Pedigrees anhand einer CSV-Datenbank.
 """
+# Imports
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import messagebox
